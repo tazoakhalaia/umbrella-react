1f89f20b0a751d6c426bf40fbf551c9919cbfb68
tazoakhalaia <darknemiroff@gmail.com>
2023-06-15 15:41:25 +0400

    admin page create products inputs

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import AdminPage from './pages/AdminPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter> 
   <Routes>
   <Route path='/' element={<MainPage />} />
+  <Route path='/admin-panel' element={<AdminPage />} />
   </Routes>
   </BrowserRouter>
 );
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function MainPage() {
-  return (
-    <div>MainPage</div>
-  )
-}
+function MainPage () {
+    return (
+        <div>
+        <Link to='#'><h1>hello</h1></Link>
+        </div>
+        )
+    };
 
 export default MainPage
